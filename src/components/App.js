cd6c4d0596a1a0cfc3f7397614c1faf9fbda8409
Vastayaa <2.mrakh@mail.ru>
2023-12-28 10:52:38 +0300

    release

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,25 @@
 import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 export const App = () => {
-  const APIurl = '90d8504af960130f95fcac51a03ffe5d';
   const [cityName, setCityName] = useState("");
   const [weatherData, setWeatherData] = useState({});
   const [isError, setIsError] = useState(false);
   const [idError, setIdError] = useState(-1);
   const changeCityInput = (e) => { setCityName(e.target.value) };
-  const fetchWeatherAPI = async() => {
+  const fetchProxyAPI = async() => {
     try {
-      let resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIurl}`);
-      if (resp.status === 200) { let respJson = await resp.json(); setWeatherData(respJson); setIsError(false) }
-      else { setIdError(resp.status); setIsError(true) } } catch (e) { setIsError(true); setIdError(599) };
+      let resp = await axios.post('http://localhost:5000/api', { text: cityName });
+      resp.data.state === 200? setWeatherData(resp.data.data) : setIsError(true); setIdError(resp.data.state);
+    } catch(e) { setIsError(true); setIdError(599) }
   }
 
-  let onEnter = (e) => { if (e.keyCode === 13 && cityName.length > 0) { fetchWeatherAPI() } else { setWeatherData("undefined"); setIsError(false) } };
+  let onEnter = (e) => { if (e.keyCode === 13 && cityName.length > 0) { fetchProxyAPI() } else { setWeatherData("undefined"); setIsError(false) } };
 
   let dateBuilder = () => {
     let d = new Date(), months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
     days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"], day = days[d.getDay()], date = d.getDate(), month = months[d.getMonth()], year = d.getFullYear();
-
     return `${day}, ${date} ${month} ${year}`;
   }
 
@@ -38,12 +37,12 @@ export const App = () => {
             <>
               <div className="locationBox">
                 <div className="location">{`${weatherData.name}, ${weatherData.sys.country}`}</div>
-                <div className="date">{dateBuilder()}</div>
+                <div className="date">{ dateBuilder() }</div>
               </div>
   
               <div className="weatherBox">
-                <div className="temp">{Math.round(weatherData.main.temp-273.15)}°c</div>
-                <div className="weather">{weatherData.weather[0].main}</div>
+                <div className="temp">{ Math.round(weatherData.main.temp-273.15) }°c</div>
+                <div className="weather">{ weatherData.weather[0].main }</div>
               </div>
             </>
           )
